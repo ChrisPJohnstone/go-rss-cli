fedbe5e827685b1b569758b4532815eb1f5d5de2
Christopher Johnstone <chrisj97@proton.me>
2026-07-06 22:56:14 +0100

    feat: added loadSharedFlags to feed subcommand

--- cmd/feed/feed.go
+++ cmd/feed/feed.go
@@ -3,11 +3,30 @@ package feed
 import "github.com/spf13/cobra"
 
 var FeedCmd = &cobra.Command{
-	Use:   "feed [command]",
-	Short: "Feed management commands",
-	Args:  cobra.MaximumNArgs(1),
+	Use:               "feed [command]",
+	Short:             "Feed management commands",
+	Args:              cobra.MaximumNArgs(1),
+	PersistentPreRunE: loadSharedFlags,
 }
+
+var (
+	verbose bool
+	toolDir string
+)
 
 func init() {
 	FeedCmd.AddCommand(addFeedCmd, rmFeedCmd)
+}
+
+func loadSharedFlags(cmd *cobra.Command, args []string) error {
+	var err error
+	toolDir, err = cmd.Flags().GetString("toolDir")
+	if err != nil {
+		return err
+	}
+	verbose, err = cmd.Flags().GetBool("verbose")
+	if err != nil {
+		return err
+	}
+	return nil
 }
